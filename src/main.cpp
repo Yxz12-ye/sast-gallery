88c9939f7570c3d10b38cd7927f5010b21c94ac1
Mairon <3043462073@qq.com>
2025-01-23 02:02:30 +0800

    feat(ci): windows/linux build and deploy

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
-    QApplication::setWindowIcon(QIcon(":/res/icon/app.svg"));
+    QApplication::setWindowIcon(QIcon(":/res/icon/app_icon.svg"));
     initSettings();
     ElaApplication::getInstance()->init();
     ElaTheme::getInstance()->setThemeMode(
