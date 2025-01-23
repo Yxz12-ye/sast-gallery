b052ce73b8507590ed2460f1e505aa9cd9ca1763
cEvolve05 <cEvolve05@gmail.com>
2025-01-23 23:42:50 +0800

    refactor: update `MediaListModel`

--- src/model/MediaListModel.cpp
+++ src/model/MediaListModel.cpp
@@ -1,47 +1,113 @@
 #include "MediaListModel.h"
-#include "MediaItem.h"
+#include <QDir>
 
-MediaListModel::MediaListModel(QList<MediaItem>&& data, QObject* parent)
-    : QAbstractListModel(parent)
-    , mediaList(std::move(data)) {}
+MediaListModel::MediaListModel(QObject* parent)
+    : QAbstractTableModel(parent) {}
+
+MediaListModel::~MediaListModel() {}
 
 int MediaListModel::rowCount(const QModelIndex& parent) const {
-    return mediaList.size();
+    return path.size();
+}
+
+int MediaListModel::columnCount(const QModelIndex& parent) const {
+    return 3;
+}
+
+QVariant MediaListModel::data(const QModelIndex& index, int role) const {
+    if (!index.isValid() || index.row() >= rowCount() || index.column() >= columnCount()) {
+        return {};
+    }
+
+    if (role == Qt::DisplayRole || role == Qt::EditRole) {
+        switch (Property(index.column())) {
+        case Property::Path:
+            return path.value(index.row());
+        case Property::LastModifiedTime:
+            return lastModifiedTime.value(index.row());
+        case Property::IsFavorite:
+            return isFavorite.contains(path.value(index.row()));
+        }
+    }
+    return {};
 }
 
 Qt::ItemFlags MediaListModel::flags(const QModelIndex& index) const {
     if (!index.isValid()) {
         return Qt::ItemIsEnabled;
     }
-
-    return QAbstractListModel::flags(index) | Qt::ItemIsEditable;
+    switch (Property(index.column())) {
+    case Property::Path:
+    case Property::LastModifiedTime:
+        return QAbstractTableModel::flags(index);
+    case Property::IsFavorite:
+        return QAbstractTableModel::flags(index) | Qt::ItemIsEditable;
+    }
 }
 
-QVariant MediaListModel::data(const QModelIndex& index, int role) const {
-    if (!index.isValid() || index.row() >= mediaList.size()) {
+QVariant MediaListModel::headerData(int section, Qt::Orientation orientation, int role) const {
+    if (orientation != Qt::Horizontal || section >= columnCount()) {
         return {};
     }
-
-    if (role == Qt::DisplayRole || role == Qt::EditRole) {
-        return QVariant::fromValue(mediaList.value(index.row()));
+    switch (Property(section)) {
+    case Property::Path:
+        return "Path";
+    case Property::LastModifiedTime:
+        return "LastModifiedTime";
+    case Property::IsFavorite:
+        return "IsFavorite";
     }
-
-    return {};
 }
 
 bool MediaListModel::setData(const QModelIndex& index, const QVariant& value, int role) {
-    if (index.isValid() && role == Qt::EditRole) {
-        mediaList.replace(index.row(), value.value<MediaItem>());
-        emit dataChanged(index, index, {role});
-        return true;
+    if (!index.isValid() || index.row() >= rowCount() || index.column() != Property::IsFavorite) {
+        return false;
+    }
+    if (role == Qt::EditRole) {
+        if (value.value<bool>()) {
+            isFavorite.insert(path.value(index.row()));
+            dataChanged(index, index);
+        } else {
+            isFavorite.remove(path.value(index.row()));
+            dataChanged(index, index);
+        }
     }
     return false;
 }
 
-void MediaListModel::reset(QList<MediaItem>&& data) {
+void MediaListModel::resetEntries(const QStringList& paths) {
     beginResetModel();
-
-    mediaList = std::move(data);
-
+    path = paths;
+    lastModifiedTime.clear();
+    for (auto& filePath : path) {
+        lastModifiedTime += QFileInfo(filePath).lastModified();
+    }
     endResetModel();
+}
+
+void MediaListModel::appendEntries(const QStringList& paths) {
+    beginInsertRows({}, path.size(), path.size() + paths.size() - 1);
+    path += paths;
+    for (auto& filePath : paths) {
+        lastModifiedTime += QFileInfo(filePath).lastModified();
+    }
+    endInsertRows();
+}
+
+void MediaListModel::removeEntries(const QStringList& paths) {
+    for (auto& filePath : paths) {
+        auto row = path.indexOf(filePath);
+        beginRemoveRows({}, row, row);
+        path.remove(row);
+        lastModifiedTime.remove(row);
+        endRemoveRows();
+    }
+}
+
+void MediaListModel::modifiedEntries(const QStringList& paths) {
+    for (auto& filePath : paths) {
+        auto row = path.indexOf(filePath);
+        lastModifiedTime.replace(row, QFileInfo(filePath).lastModified());
+        dataChanged(index(row, Property::LastModifiedTime), index(row, Property::LastModifiedTime));
+    }
 }
--- src/model/MediaListModel.h
+++ src/model/MediaListModel.h
@@ -1,25 +1,33 @@
 #pragma once
 
-#include "MediaItem.h"
-#include <QAbstractListModel>
+#include <QAbstractTableModel>
+#include <QDateTime>
 #include <QList>
 
 // model for storing media property on disk, not including the image content
-
-class MediaListModel : public QAbstractListModel {
+// TODO: load & save favorite data from disk
+class MediaListModel : public QAbstractTableModel {
     Q_OBJECT
+    enum Property { Path = 0, LastModifiedTime = 1, IsFavorite = 2 };
 
-    QList<MediaItem> mediaList;
+    QList<QString> path;
+    QList<QDateTime> lastModifiedTime;
+    QSet<QString> isFavorite;
 
 public:
-    MediaListModel(QList<MediaItem>&& data, QObject* parent = nullptr);
+    MediaListModel(QObject* parent = nullptr);
+    ~MediaListModel();
 
-    int rowCount(const QModelIndex& parent) const override;
-    Qt::ItemFlags flags(const QModelIndex& index) const override;
+    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+    int columnCount(const QModelIndex& parent = QModelIndex()) const override;
     QVariant data(const QModelIndex& index, int role) const override;
+    Qt::ItemFlags flags(const QModelIndex& index) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
     bool setData(const QModelIndex& index, const QVariant& value, int role) override;
 
-    // TODO: add item
-    // TODO: del item
-    void reset(QList<MediaItem>&& data);
+    // for signals from disk scanner
+    void resetEntries(const QStringList& paths);
+    void appendEntries(const QStringList& paths);
+    void removeEntries(const QStringList& paths);
+    void modifiedEntries(const QStringList& paths);
 };
