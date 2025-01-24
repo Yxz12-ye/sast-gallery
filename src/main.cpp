7b15c083eac9fc5af4a4ada632cd4ad8c794c764
Mairon <3043462073@qq.com>
2025-01-24 18:13:53 +0800

    feat: add windows manifest file for application compatibility

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,8 @@ int main(int argc, char* argv[]) {
     QApplication::setWindowIcon(QIcon(":/res/icon/app_icon.svg"));
     initSettings();
     ElaApplication::getInstance()->init();
-    // ElaTheme::getInstance()->setThemeMode(
-    //     settings.value("theme").toString() == "light" ? ElaThemeType::Light : ElaThemeType::Dark);
+    ElaTheme::getInstance()->setThemeMode(
+        settings.value("theme").toString() == "light" ? ElaThemeType::Light : ElaThemeType::Dark);
     MainWindow w;
     w.show();
     return a.exec();
