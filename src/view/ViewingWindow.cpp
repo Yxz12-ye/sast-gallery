3bdf79e81965b069c8ff2223ee6edf38b5c08d94
wyq777 <635118061@qq.com>
2025-01-25 16:23:17 +0800

    feat: provide connect&signals without actual slots in `ViewingWindow`

--- src/view/ViewingWindow.cpp
+++ src/view/ViewingWindow.cpp
@@ -27,15 +27,15 @@ void ViewingWindow::initContent() {
 
     // Create menu bar
     ElaMenuBar* menuBar = new ElaMenuBar(this);
-    menuBar->addElaIconAction(ElaIconType::RotateRight, "rotate");
-    menuBar->addElaIconAction(ElaIconType::TrashCan, "delete");
-    menuBar->addElaIconAction(ElaIconType::Print, "print");
-    menuBar->addElaIconAction(ElaIconType::Pen, "edit");
+    QAction* rotateAction = menuBar->addElaIconAction(ElaIconType::RotateRight, "rotate");
+    QAction* deleteAction = menuBar->addElaIconAction(ElaIconType::TrashCan, "delete");
+    QAction* printAction = menuBar->addElaIconAction(ElaIconType::Print, "print");
+    QAction* editAction = menuBar->addElaIconAction(ElaIconType::Pen, "edit");
     ElaMenu* fileMenu = menuBar->addMenu(ElaIconType::Ellipsis, nullptr);
-    fileMenu->addAction("Open");
-    fileMenu->addAction("Copy");
-    fileMenu->addAction("Save As");
-    fileMenu->addAction("Open image in File Explorer");
+    QAction* openFileAction = fileMenu->addAction("Open");
+    QAction* copyFileAction = fileMenu->addAction("Copy");
+    QAction* saveasFileAction = fileMenu->addAction("Save As");
+    QAction* openInFileExplorerAction = fileMenu->addAction("Open image in File Explorer");
     fileMenu->addSeparator();
 
     //Set menu bar layout policy
@@ -62,27 +62,38 @@ void ViewingWindow::initContent() {
 
     // Create buttons
     QHBoxLayout* operationLayout = new QHBoxLayout(this);
+
     ElaIconButton* likeButton = new ElaIconButton(ElaIconType::Heart);
     likeButton->setMaximumWidth(25);
+
     ElaIconButton* fileInfoButton = new ElaIconButton(ElaIconType::CircleInfo);
     fileInfoButton->setMaximumWidth(25);
+
     ElaText* dividerText1 = new ElaText("|", this);
     dividerText1->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
     dividerText1->setTextPixelSize(14);
+
     ElaText* fileInfoBriefText = new ElaText("1920 x 1080 1.2MB", this);
+
     fileInfoBriefText->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
     fileInfoBriefText->setTextPixelSize(14);
+
     ElaIconButton* zoomInButton = new ElaIconButton(ElaIconType::MagnifyingGlassPlus);
     zoomInButton->setMaximumWidth(25);
+
     ElaSlider* zoomSlider = new ElaSlider(Qt::Orientation::Horizontal);
     zoomSlider->setMaximumWidth(200);
+
     ElaIconButton* zoomOutButton = new ElaIconButton(ElaIconType::MagnifyingGlassMinus);
     zoomOutButton->setMaximumWidth(25);
+
     ElaText* dividerText2 = new ElaText("|", this);
     dividerText2->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
     dividerText2->setTextPixelSize(14);
+
     ElaIconButton* fullscreenButton = new ElaIconButton(ElaIconType::ExpandWide);
     fullscreenButton->setMaximumWidth(25);
+
     ElaIconButton* zoom2originalButton = new ElaIconButton(ElaIconType::Expand);
     zoom2originalButton->setMaximumWidth(25);
 
@@ -111,4 +122,57 @@ void ViewingWindow::initContent() {
     QWidget* centralWidget = new QWidget(this);
     centralWidget->setLayout(viewLayout);
     setCentralWidget(centralWidget);
+
+    // show actions info when pointing at them
+    openFileAction->setStatusTip("Open a file");
+    copyFileAction->setStatusTip("Copy a file");
+    saveasFileAction->setStatusTip("Save a file as");
+    openInFileExplorerAction->setStatusTip("Open the file in File Explorer");
+    rotateAction->setStatusTip("Rotate the image");
+    deleteAction->setStatusTip("Delete the image");
+    printAction->setStatusTip("Print the image");
+    editAction->setStatusTip("Edit the image");
+    likeButton->setStatusTip("Like the image");
+    fileInfoButton->setStatusTip("Show file info");
+    zoomInButton->setStatusTip("Zoom in");
+    zoomOutButton->setStatusTip("Zoom out");
+    fullscreenButton->setStatusTip("Fullscreen");
+    zoom2originalButton->setStatusTip("Zoom to original size");
+
+    // connect to actions
+    connect(openFileAction, &QAction::triggered, this, [=]() { qDebug() << "Open file clicked"; });
+    connect(copyFileAction, &QAction::triggered, this, [=]() { qDebug() << "Copy file clicked"; });
+    connect(saveasFileAction, &QAction::triggered, this, [=]() {
+        qDebug() << "Save as file clicked";
+    });
+    connect(openInFileExplorerAction, &QAction::triggered, this, [=]() {
+        qDebug() << "Open in file explorer clicked";
+    });
+    connect(rotateAction, &QAction::triggered, this, [=]() { qDebug() << "Rotate action clicked"; });
+    connect(deleteAction, &QAction::triggered, this, [=]() { qDebug() << "Delete action clicked"; });
+    connect(printAction, &QAction::triggered, this, [=]() { qDebug() << "Print action clicked"; });
+    connect(editAction, &QAction::triggered, this, [=]() { qDebug() << "Edit action clicked"; });
+
+    connect(likeButton, &ElaIconButton::clicked, this, [=]() { qDebug() << "Like button clicked"; });
+    connect(fileInfoButton, &ElaIconButton::clicked, this, [=]() {
+        qDebug() << "File info button clicked";
+    });
+    connect(zoomInButton, &ElaIconButton::clicked, this, [=]() {
+        qDebug() << "Zoom in button clicked";
+    });
+    connect(zoomOutButton, &ElaIconButton::clicked, this, [=]() {
+        qDebug() << "Zoom out button clicked";
+    });
+    connect(fullscreenButton, &ElaIconButton::clicked, this, [=]() {
+        qDebug() << "Fullscreen button clicked";
+    });
+    connect(zoom2originalButton, &ElaIconButton::clicked, this, [=]() {
+        qDebug() << "Zoom to original button clicked";
+    });
+    connect(zoomSlider, &ElaSlider::valueChanged, this, [=](int value) {
+        qDebug() << "Zoom slider value changed to " << value;
+    });
+    connect(scene, &ElaGraphicsScene::mouseLeftClickedItem, this, [=](ElaGraphicsItem* item) {
+        qDebug() << "Scene now displays image";
+    });
 }
